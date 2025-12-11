7ffb52f7d330530cc558fd2154f7ac5552bf1b99
tensorbytes0202 <aditya11501246@gmail.com>
2025-12-12 02:02:58 +0530

    first commit

---
+++ ingestion_pipelines.py
@@ -0,0 +1,15 @@
+import os 
+from langchain_community.document_loaders import TextLoader,DirectoryLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+
+def main():
+    print("Main Function")
+     
+if __name__ =="__main__":
+         main()
